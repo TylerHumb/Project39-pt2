285e4e9b5d8a6476a32e9398863bff09a63ea40b
Roma-Thakkar <s3947657@student.rmit.edu.au>
2022-09-06 16:02:35 +1000

    Front Door and Toilet Door Animations done
    Animations for two doors are done

---
+++ Project 39/Assets/TDoor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TDoor : MonoBehaviour
+{
+  public bool isAuto;
+  public Animator anim;
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if(other.tag == "Player")
+    {
+      anim.SetBool("Open", true);
+    }
+  }
+
+  private void OnTriggerExit(Collider other)
+  {
+    if(other.tag == "Player")
+    {
+      anim.SetBool("Open", false);
+    }
+  }
+
+  void Start()
+  {
+
+  }
+  void Update()
+  {
+
+  }
+}
